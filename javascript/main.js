55993daeea38b0595595a75309653a62e9350534
Pedro Henrique Cunha Moda <phcunha47@gmail.com>
2023-10-01 00:49:22 -0300

    Lading Page 70%

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,14 @@
 function updateElementOnScroll() {
     const navElement = document.querySelector('nav');
     if (window.scrollY === 0) {
-        document.documentElement.style.setProperty('--cor-de-fundo-nav', 'transparent');
+        navElement.style.setProperty('--cor-de-fundo-nav', 'transparent');
+        navElement.style.setProperty('--cor-links-nav', 'white');
     } else {
-        document.documentElement.style.setProperty('--cor-de-fundo-nav', 'white');
-    }
+        navElement.style.setProperty('--cor-de-fundo-nav', 'white');
+        navElement.style.setProperty('--cor-links-nav', '#333');
+    } 
 }
 
 window.addEventListener('scroll', updateElementOnScroll);
 
 updateElementOnScroll();
-
-
-
-
-
-
